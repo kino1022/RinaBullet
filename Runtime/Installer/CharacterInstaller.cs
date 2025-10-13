e968ecb4e5e25ff4857fc2fa19b1cde19e2def16
kino1022 <yuta_fujita@outlook.com>
2025-10-13 21:17:33 +0900

    createInstaller

---
+++ Runtime/Installer/CharacterInstaller.cs
@@ -0,0 +1,37 @@
+using RinaBullet.Context.Container;
+using RinaBullet.Shooter.Interface;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using VContainer;
+using VContainer.Unity;
+
+namespace RinaBullet.Installer {
+    public class CharacterInstaller : SerializedMonoBehaviour, IInstaller {
+
+        [OdinSerialize]
+        [LabelText("弾丸射出コンポーネント")]
+        private IBulletShooter m_shooter;
+        
+        [OdinSerialize]
+        [LabelText("コンテキスト管理コンポーネント")]
+        private IContextContainer m_contextContainer;
+
+        public void Install(IContainerBuilder builder) {
+            if (m_shooter == null) {
+                m_shooter = gameObject.transform.root.GetComponentInChildren<IBulletShooter>();
+            }
+
+            builder
+                .RegisterComponent(m_shooter)
+                .As<IBulletShooter>();
+
+            if (m_contextContainer == null) {
+                m_contextContainer = gameObject.transform.root.GetComponentInChildren<IContextContainer>();
+            }
+            
+            builder
+                .RegisterComponent(m_contextContainer)
+                .As<IContextContainer>();
+        }
+    }
+}
